0786b36e524d8b0cac07d8d5637f5c0f3f1046fe
riddhiraj <87946275+RiddhiRaj@users.noreply.github.com>
2022-09-22 20:31:51 +0530

    .

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ import styles from "./styles.module.scss";
 const Header = () => (
     <header className={styles["header"]}>
         <div className={styles["header__title"]}>
-            Project made to show that files can be stored in a distributed file system and their hashes in the blockchain with some basic metadata.
+            Project made to show that files can be stored in a distributed file system(IPFS) & their hashes in the blockchain with some basic metadata.
         </div>
         <div className={styles["header__subtitle"]}>Following technologies have been used: Solidity, IPFS, Truffle, React(UI)</div>
         <div className={styles["header__images"]}>
